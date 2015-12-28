da36a3ed28cf1caaaa23f33e788d0afe553c0acc
Bianca Gibson <bianca.gibson@rea-group.com>
2015-12-28 15:21:03 +1100

    first fib implementations

---
+++ fib.rb
@@ -0,0 +1,21 @@
+def mutableFib(n)
+  if n == 0 || n == 1
+    1
+  else
+    prev = 1
+    curr = 1
+    [2, 3, 4, 5].each do
+      puts curr
+      oldcurr = curr
+      curr = prev + curr
+      prev = oldcurr
+    end
+    curr
+  end
+end
+
+def recursiveFib(n)
+  n == 0 || n == 1 ? 1 : recursiveFib(n - 1) + recursiveFib(n - 2)
+end
+
+
