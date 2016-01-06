12a6b67dc6b074674b39abe23037a2a4ce57b963
Bianca Gibson <bianca.gibson@rea-group.com>
2016-01-07 10:55:50 +1100

    Fix immutability example

--- immutability_fib.rb
+++ immutability_fib.rb
@@ -4,8 +4,7 @@ def mutableFib(n)
   else
     prev = 1
     curr = 1
-    [2, 3, 4, 5].each do
-      puts curr
+    (2..n).each do
       oldcurr = curr
       curr = prev + curr
       prev = oldcurr
@@ -16,10 +15,4 @@ end
 
 def recursiveFib(n)
   n == 0 || n == 1 ? 1 : recursiveFib(n - 1) + recursiveFib(n - 2)
-end
-
-def reduceFib(n)
-  def base_case(n)
-    n == 0 || n == 1
-  end
 end
