90fca613ab9f8c7ff9c12a2d46b35645911c647c
Bianca Gibson <bianca.gibson@rea-group.com>
2016-01-07 14:35:58 +1100

    Remove logging that's useless

--- mutable_reverse.rb
+++ mutable_reverse.rb
@@ -1,13 +1,11 @@
 def transparent_example
   a = "Hello, world!"
   puts a.reverse
-  puts "a is: #{a}"
   puts a.reverse
 end
 
 def mutable_example
   a = "Hello, world!"
   puts a.reverse!
-  puts "a is: #{a}"
   puts a.reverse!
 end
