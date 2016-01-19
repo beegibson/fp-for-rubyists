ca1411f86220647677d3856d858e0d6109184144
Bianca Gibson <bianca.gibson@rea-group.com>
2016-01-19 22:03:43 +1100

    add total example

---
+++ side_effects_total.rb
@@ -0,0 +1,24 @@
+require 'logger'
+
+def mutable_total_cart(items, logger: Logger.new)
+  total = 0
+  items.each do |item|
+    total += item.cost
+  end
+  logger.info(total)
+end
+
+def total_cart(items)
+  items.inject(0) { |total, item| total = total + item.cost}
+end
+
+def print_total_cart(items, logger: Logger.new)
+  logger.info(total_cart(items))
+end
+
+class Item
+  attr_reader :cost
+  def initialize(cost)
+    @cost = cost
+  end
+end
